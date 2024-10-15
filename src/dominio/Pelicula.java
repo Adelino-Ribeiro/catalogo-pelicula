0e9cb72fd833bb5063a6bf17a6c621c5c8318425
Adelino Ribeiro <adelinomiguelribeiro95@gmail.com>
2024-10-16 00:58:45 +0100

    primeiro  commit

---
+++ src/dominio/Pelicula.java
@@ -0,0 +1,58 @@
+package dominio;
+
+public class Pelicula {
+
+    private String nome;
+
+    public Pelicula () {}
+
+    public Pelicula(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pelicula other = (Pelicula) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula [nome=" + nome + "]";
+    }
+
+   public static void main (String[] ags) {
+    var pelicula1 = new Pelicula("Batman");
+    var pelicula2 = new Pelicula("Superman");
+
+    System.out.println(pelicula1);
+    System.out.println(pelicula2);
+   }
+}
